eba5076c4ecb1b154d0ae50e3fdd88352c3f5dbe
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-08-09 09:39:53 +0530

    Create 1768. Merge Strings Alternately.py

---
+++ Day 8 String/1768. Merge Strings Alternately.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans=''
+        for i in range(min(len(word1),len(word2))):
+            ans += word1[i] + word2[i]
+        return ans + word1[i+1:] + word2[i+1:]
